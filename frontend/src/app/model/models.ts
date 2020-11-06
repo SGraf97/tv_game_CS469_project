9b7d737c4458db132b0e54a33190749fe5564887
ASTERIOS <csd3978@csd.uoc.gr>
2020-11-07 01:02:26 +0200

    typescript for models

---
+++ frontend/src/app/model/models.ts
@@ -0,0 +1,15 @@
+export class Model {
+  name: string;
+  age: number;
+  height: number;
+  points: number;
+
+  constructor(name, age, height, points) { 
+    this.name = name;
+    this.age = age;
+    this.height = height;
+    this.points = points;
+  }
+
+
+}
