7d4d1541910abc4492cf895e9228580a0fe2df1b
spyros stafylarakis <sstaf97@gmail.com>
2020-11-11 08:59:24 +0200

    stageComit

--- frontend/src/app/main-display/starting-main-screen/starting-main-screen.component.ts
+++ frontend/src/app/main-display/starting-main-screen/starting-main-screen.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {User} from '../../model/user';
-import { CommonModule } from '@angular/common';
+import {FrameComponent} from '../frame/frame.component';
+
 @Component({
   selector: 'app-starting-main-screen',
   templateUrl: './starting-main-screen.component.html',
@@ -8,11 +9,21 @@ import { CommonModule } from '@angular/common';
 })
 export class StartingMainScreenComponent implements OnInit {
   onlineUsers: any;
+  nextEpisode: any;
   constructor() { }
 
   ngOnInit(): void {
       this.onlineUsers = User.getUsers();
+      this.nextEpisode = '20/12/20';
+      document.querySelector('#startEpisode').addEventListener('click' , () => {
+
+        const a = document.querySelector('app-main-display') as any ;
+        console.log(a);
+        a.querySelector('app-starting-main-screen').style.display = 'none';
+        console.log(FrameComponent);
+      });
 
   }
+
 
 }
