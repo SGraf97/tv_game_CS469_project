cba9748586fc33e5eedaddf1eb752c1cffb45a7c
ASTERIOS <csd3978@csd.uoc.gr>
2020-11-12 04:54:46 +0200

    routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,14 +1,20 @@
 import {NgModule, OnInit} from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UsersInfoComponent } from './wall/users-info/users-info.component';
+import { ModelsInfoComponent } from './wall/models-info/models-info.component';
+import { WallComponent } from './wall/wall.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: WallComponent },
+  { path: 'models-info', component: ModelsInfoComponent },
+  { path: 'users-info', component: UsersInfoComponent }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
-
-
 }
 
