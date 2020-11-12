b196df86eaaa6d493a7806997cab21891b67740e
ASTERIOS <csd3978@csd.uoc.gr>
2020-11-12 04:58:04 +0200

    users

--- frontend/src/app/model/user.ts
+++ frontend/src/app/model/user.ts
@@ -5,11 +5,15 @@ export class User {
   password: string;
   color: string;
   photoProfile: string;
+  xp: number;
+  level: number;
 
 
   constructor(username: any, color: any) {
     this.username = username;
     this.color = color;
+    this.xp=0;
+    this.level=0;
   }
 
 
