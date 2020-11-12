eada90d65968e52ad364fd165c8f5c6e74c667fa
ASTERIOS <csd3978@csd.uoc.gr>
2020-11-12 04:58:49 +0200

    html for wall

--- frontend/src/app/wall/wall.component.ts
+++ frontend/src/app/wall/wall.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit} from '@angular/core';
+import { Router } from '@angular/router';
 import {Model} from '../model/models';
 
 @Component({
@@ -10,16 +11,20 @@ export class WallComponent implements OnInit {
  
   models = [];
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
     
     this.models = [
       new Model("ΗΡΑΚΛΗΣ", 19, 177, 116),
-      new Model("ΙΡΙΔΑ", 19, 177, 96),
-      new Model("ΜΕΓΚΥ", 19, 177, 85)
+      new Model("ΙΡΙΔΑ", 21, 177, 96),
+      new Model("ΜΕΓΚΥ", 23, 177, 85)
     ];
   }
+
+  /*onSelect() {
+    this.router.navigateByUrl(['/models-info', model]);  // define your component where you want to go
+  }*/
 
   /*Tha einai mia synartisi pou tha epistrefei enan pinaka 3 thesewn (top 3 models) kai tha ta emfanizei ston toixo
   topModels(): string[] {
