aee001461b6091e96766a5fa83f7e01815032fbd
ASTERIOS <csd3978@csd.uoc.gr>
2020-11-12 04:56:07 +0200

    module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainDisplayComponent } from './main-display/main-display.component';
@@ -8,6 +10,11 @@ import { WallComponent } from './wall/wall.component';
 import { TableComponent } from './table/table.component';
 import { PhoneComponent } from './phone/phone.component';
 import { ChatComponent } from './main-display/chat/chat.component';
+import { TwitterComponent } from './main-display/twitter/twitter.component';
+import { BuzzerComponent } from './table/buzzer/buzzer.component';
+import { OptionComponent } from './table/option/option.component';
+import { UsersInfoComponent } from './wall/users-info/users-info.component';
+import { ModelsInfoComponent } from './wall/models-info/models-info.component';
 
 @NgModule({
   declarations: [
@@ -16,11 +23,17 @@ import { ChatComponent } from './main-display/chat/chat.component';
     WallComponent,
     TableComponent,
     PhoneComponent,
-    ChatComponent
+    ChatComponent,
+    TwitterComponent,
+    BuzzerComponent,
+    OptionComponent,
+    UsersInfoComponent,
+    ModelsInfoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
