89c9f74dbbe876555975f8e078411f833ebad98c
ASTERIOS <csd3978@csd.uoc.gr>
2020-11-12 05:01:18 +0200

    user's info

---
+++ frontend/src/app/wall/users-info/users-info.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UsersInfoComponent } from './users-info.component';
+
+describe('UsersInfoComponent', () => {
+  let component: UsersInfoComponent;
+  let fixture: ComponentFixture<UsersInfoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ UsersInfoComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UsersInfoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/wall/users-info/users-info.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../model/user';
+
+@Component({
+  selector: 'app-users-info',
+  templateUrl: './users-info.component.html',
+  styleUrls: ['./users-info.component.css']
+})
+export class UsersInfoComponent implements OnInit {
+  users: any;
+  constructor() { }
+
+  ngOnInit(): void {
+    //needs users that already exist somewhere
+    this.users = [ 
+      new User('user1', 'red'),
+      new User('User2', 'blue')
+    ]; 
+  }
+}
