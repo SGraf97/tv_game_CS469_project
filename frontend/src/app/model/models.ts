49e7ae4d4f26ec88d4af04f1cda4b95e7ed0c5f0
ASTERIOS <csd3978@csd.uoc.gr>
2020-11-12 04:57:15 +0200

    for models

--- frontend/src/app/model/models.ts
+++ frontend/src/app/model/models.ts
@@ -3,12 +3,14 @@ export class Model {
   age: number;
   height: number;
   points: number;
+  description : string;
 
   constructor(name, age, height, points) { 
     this.name = name;
     this.age = age;
     this.height = height;
     this.points = points;
+    this.description = "Born in Thessaloniki, she started modeling two years ago. She is studying modeling and at the same time, she is working on it.";
   }
 
 
