32e6778373a5d90c23faf727c92145a6d28ea5ca
ASTERIOS <csd3978@csd.uoc.gr>
2020-11-17 02:18:56 +0200

    routing module

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import {NgModule, OnInit} from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TwitterComponent } from './main-display/twitter/twitter.component';
 import { UsersInfoComponent } from './wall/users-info/users-info.component';
 import { ModelsInfoComponent } from './wall/models-info/models-info.component';
+import { TonightHighlightsComponent } from './wall/tonight-highlights/tonight-highlights.component';
 import { WallComponent } from './wall/wall.component';
+import { ChatComponent } from './main-display/chat/chat.component';
 
 const routes: Routes = [
-  { path: '', component: WallComponent },
+  { path: '', component: TwitterComponent },
+  { path: 'tonight-highlights', component: TonightHighlightsComponent },
   { path: 'models-info', component: ModelsInfoComponent },
   { path: 'users-info', component: UsersInfoComponent }
   
