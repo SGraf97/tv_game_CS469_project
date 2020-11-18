33ddf21e67a6ec1f715b2eff63683d94599dd6b3
ASTERIOS <csd3978@csd.uoc.gr>
2020-11-18 03:03:31 +0200

    ts for model's info

--- frontend/src/app/wall/models-info/models-info.component.ts
+++ frontend/src/app/wall/models-info/models-info.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import {Model} from '../../model/models';
 
 @Component({
   selector: 'app-models-info',
@@ -7,10 +8,11 @@ import { Router } from '@angular/router';
   styleUrls: ['./models-info.component.css']
 })
 export class ModelsInfoComponent implements OnInit {
-
+  models = [];
   constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.models = Model.getModels();
   }
 
 }
