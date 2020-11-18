e4588c386c3e05782ddb84dd95277f35de80b27f
ASTERIOS <csd3978@csd.uoc.gr>
2020-11-18 03:04:33 +0200

    ts for highlights

--- frontend/src/app/wall/tonight-highlights/tonight-highlights.component.ts
+++ frontend/src/app/wall/tonight-highlights/tonight-highlights.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TonightHighlightsComponent implements OnInit {
 
+  title1: string;
+  title2: string;
   constructor() { }
 
+  
   ngOnInit(): void {
+    /*(get title from iframe)
+    
+    let iframe = document.getElementById('video1') as HTMLIFrameElement;
+    this.title1 = iframe.contentDocument.title;
+    console.log(this.title1);*/
+
+   
   }
+
+ 
 
 }
