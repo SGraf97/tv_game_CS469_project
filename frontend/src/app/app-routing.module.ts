343136cd53091387b16991028267f2eb0615e032
ASTERIOS <csd3978@csd.uoc.gr>
2020-11-18 03:01:30 +0200

    routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,14 +5,15 @@ import { UsersInfoComponent } from './wall/users-info/users-info.component';
 import { ModelsInfoComponent } from './wall/models-info/models-info.component';
 import { TonightHighlightsComponent } from './wall/tonight-highlights/tonight-highlights.component';
 import { WallComponent } from './wall/wall.component';
+import { WallNotificationComponent } from './wall/wall-notification/wall-notification.component';
 // import { ChatComponent } from './main-display/chat/chat.component';
 
 const routes: Routes = [
   { path: 'wall', component: WallComponent },
-  { path: 'tonight-highlights', component: TonightHighlightsComponent },
-  { path: 'models-info', component: ModelsInfoComponent },
-  { path: 'users-info', component: UsersInfoComponent }
-  
+  { path: 'wall/tonight-highlights', component: TonightHighlightsComponent },
+  { path: 'wall/models-info', component: ModelsInfoComponent },
+  { path: 'wall/users-info', component: UsersInfoComponent },
+  { path: 'wall/wall-notification', component: WallNotificationComponent}
 ];
 
 @NgModule({
