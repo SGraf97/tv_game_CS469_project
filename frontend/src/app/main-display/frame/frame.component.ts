6fe09f7a6066ded25c292a70375910ce350fa571
ASTERIOS <csd3978@csd.uoc.gr>
2020-12-06 04:42:55 +0200

    ok

--- frontend/src/app/main-display/frame/frame.component.ts
+++ frontend/src/app/main-display/frame/frame.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {SmartSpeakerService} from '../../smart-speaker.service';
 
 @Component({
   selector: 'app-frame',
@@ -10,13 +11,30 @@ export class FrameComponent implements OnInit {
 
   id : any;
   link : any;
-  constructor() { }
+  private _smartSpeaker: any;
+  constructor() {
+    this._smartSpeaker = new SmartSpeakerService();
+   }
 
   ngOnInit(): void {
 
     this.id = 'tgbNymZ7vqY';
     this.link = 'youtube.com/embed/';
     this.link += this.id;
+
+    /*this._smartSpeaker.addCommand('play', ()=>{
+      console.log('Play the video');
+      var iframe = document.getElementsByTagName("iframe");
+      var video = iframe[0].contentWindow;
+      video.postMessage('{"event":"command","func":"' + 'playVideo' +   '","args":""}', '*');
+    });*/
+
+    /*this._smartSpeaker.addCommand('pause', ()=>{
+      console.log('Stop the video');
+      var iframe = document.getElementsByTagName("iframe");
+      var video = iframe[0].contentWindow;
+      video.postMessage('{"event":"command","func":"' + 'pauseVideo' +   '","args":""}', '*');
+    });*/
 
     // this.source = '\"https://www.youtube.com/embed/3EB13m5Ng9c\";
   }
