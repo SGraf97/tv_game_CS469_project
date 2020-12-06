736b519f881c6fa98e77430c200fb73c77e2bda7
beevg <georgedakie@yahoo.com>
2020-12-06 13:22:01 +0200

    unofficial commit

--- frontend/src/app/model/user.service.ts
+++ frontend/src/app/model/user.service.ts
@@ -5,43 +5,58 @@ import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import * as _ from 'lodash';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { promise } from 'protractor';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
 
-    private hostURl: string;
+  private hostURl: string;
+  httpClient: any;
 
-    constructor(private http: HttpClient) {
-      this.hostURl = environment.host;
-    }
-  
-    public getAll(): Observable<User[]> {
-      return this.http
-        .get<User[]>(`${this.hostURl}/api/user/users`)
-        .pipe(map(result => _.map(result, (t) => new User(t.username, t.color))));
-    }
-  
-    public getByUsername(username: string): Observable<any> {
-      return this.http
-      .get(`${this.hostURl}/api/user/users/${username}`)
+  constructor(private http: HttpClient) {
+    this.hostURl = environment.host;
+  }
+
+  public getAll(): Observable<User[]> {
+    return this.http
+      .get<User[]>(`${this.hostURl}/api/user/users`)
+      .pipe(map(result => _.map(result, (t) => new User(t.username, t.color))));
+  }
+
+  public getByUsername(username: string): Observable<void> {
+    console.log(`${this.hostURl}/api/user/users/${username}`);
+    let promise = new Promise((resolve, reject) => {
+      let apiURL = `${this.hostURl}/api/user/users/${username}`;
+      this.http.get(apiURL)
+          .toPromise()
+          .then(
+              res => { // Success
+
+                console.log(res);
+                resolve();
+              },
+              msg => { // Error
+                reject(msg);
+              }
+          );
+    });
+  }
+
+  public newUser(resource: User): Observable<any> {
+    return this.http
+      .post<User>(`${this.hostURl}/api/user/user`, resource)
       .pipe(map(result => console.log(result)));
-    }
-  
-    public newUser(resource: User): Observable<any> {
-      return this.http
-        .post<User>(`${this.hostURl}/api/user/user`, resource)
-        .pipe(map(result => console.log(result)));
-    }
-  
-    public update(resource: User): Observable<any> {
-      return this.http
-        .put<User>(`${this.hostURl}/api/user/users/${resource.username}`, resource)
-        .pipe(map(result => console.log(result)));
-    }
-  
-    public delete(): Observable<void> {
-      return this.http.delete<void>(`${this.hostURl}/api/users`);
-    }
+  }
+
+  public update(resource: User): Observable<any> {
+    return this.http
+      .put<User>(`${this.hostURl}/api/user/users/${resource.username}`, resource)
+      .pipe(map(result => console.log(result)));
+  }
+
+  public delete(): Observable<void> {
+    return this.http.delete<void>(`${this.hostURl}/api/users`);
+  }
 }
