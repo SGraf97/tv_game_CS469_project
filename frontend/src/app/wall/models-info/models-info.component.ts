b3c922e31c35f00067a86c79cb97f9a2338c39b5
ASTERIOS <csd3978@csd.uoc.gr>
2020-12-06 04:45:59 +0200

    ok

--- frontend/src/app/wall/models-info/models-info.component.ts
+++ frontend/src/app/wall/models-info/models-info.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit, Input } from '@angular/core';
+import { Router, ActivatedRoute} from '@angular/router';
 import {Model} from '../../model/models';
 
 @Component({
@@ -8,11 +8,33 @@ import {Model} from '../../model/models';
   styleUrls: ['./models-info.component.css']
 })
 export class ModelsInfoComponent implements OnInit {
+  @Input() name:string;
+  @Input() age:string;
+  @Input() height:string;
   models = [];
-  constructor(private router: Router) { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    /*this.name = this.route.snapshot.paramMap.get('name');*/
+    this.route.paramMap.subscribe( params =>
+      this.name = params.get('name') 
+    ),
+    this.route.paramMap.subscribe( params =>
+      this.age = params.get('age')
+    ),
+    this.route.paramMap.subscribe( params =>
+      this.height = params.get('height')
+    )
+    
     this.models = Model.getModels();
+    this.sortArray();
+    /*console.log(this.model);*/
+  }
+
+  sortArray() {
+    this.models.sort((a, b) => parseFloat(b.points) - parseFloat(a.points));
+ 
+    /*console.log(this.models[1]);*/
   }
 
 }
